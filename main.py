628076cb51e1b59a1e907f5c07f8d70cdeee1cda
NekoCarreraDesigns <denvermaas@gmail.com>
2022-02-18 14:24:29 -0700

    added caption and title to game

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+# imports for platform game
 import pygame
 import sys
 from settings import *
 
+# pygame global variables
 pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 
 
+# caption and title
+pygame.display.set_caption("Pimp-ee-yo: The Game Is to Be Sold, Not Told")
+captionImg = pygame.image.load('pimp.png')
+pygame.display.set_icon(captionImg)
+
+# game loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
