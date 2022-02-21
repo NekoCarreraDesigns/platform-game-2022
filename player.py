25fd3df5b5ea557703482305ee6685a61565616c
NekoCarreraDesigns <denvermaas@gmail.com>
2022-02-21 12:23:33 -0700

    created gavity and jump mechanics for game

--- player.py
+++ player.py
@@ -7,8 +7,11 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((32, 64))
         self.image.fill((255, 0, 0))
         self.rect = self.image.get_rect(topleft=pos)
+        # player movement
         self.direction = pygame.math.Vector2(0, 0)
         self.speed = 4
+        self.gravity = 1
+        self.jump_speed = -1
 
     def get_input(self):
         keys = pygame.key.get_pressed()
@@ -19,9 +22,20 @@ class Player(pygame.sprite.Sprite):
         elif keys[pygame.K_LEFT]:
             self.direction.x = -1
 
+        elif keys[pygame.K_SPACE]:
+            self.jump()
+
         else:
             self.direction.x = 0
+
+    def apply_gravity(self):
+        self.direction.y += self.gravity
+        self.rect.y = self.direction.y
+
+    def jump(self):
+        self.direction.y = self.jump_speed
 
     def update(self):
         self.get_input()
         self.rect.x += self.direction.x * self.speed
+        self.apply_gravity()
