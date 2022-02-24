068e2b0601cc5556807050ed616011edc6689e53
NekoCarreraDesigns <denvermaas@gmail.com>
2022-02-24 14:24:02 -0700

    removed old level layouts

--- settings.py
+++ settings.py
@@ -1,28 +1,3 @@
-levelOne_map = [
-    '                      ',
-    '                      ',
-    '                      ',
-    'XX    XXX      XX     ',
-    'XX P                  ',
-    'XXXX        XX     XX ',
-    'XXXX      XX    XX    ',
-    'XX    X XXXX    XXX   ',
-    '      X XXXX    XXXX  ',
-    '   XXXX XXXXXX  XXXX  ',
-    'XXXXXXX XXXXXX  XXXX  ']
-
-levelTwo_map = [
-    '                      ',
-    '                      ',
-    '                      ',
-    'XXXX    XXX           ',
-    'XX P  XX     XX    XXX',
-    'XXXX      X      X    ',
-    'XXXXX    XX    X   X  ',
-    'XXX   XXXXX  X    XX  ',
-    'XXXXX       XX XXXXX  ',
-    'XXXXXXXXXXX  XX XXXXX ']
-
 vertical_tile_number = 11
 tile_size = 64
 screen_width = 1200
