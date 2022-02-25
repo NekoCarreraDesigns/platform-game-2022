29879003532471469796ff2c240770fa1f5aed63
NekoCarreraDesigns <denvermaas@gmail.com>
2022-02-25 14:42:43 -0700

    working on logic for displaying graphics

--- levels.py
+++ levels.py
@@ -3,13 +3,13 @@ from tiles import Tile
 from settings import tile_size, screen_width
 from player import Player
 # from particles import Particle_Effect
-from support import import_csv_layout
+from support import import_csv_layout, import_cut_graphics
 
 
 class Level:
     def __init__(self, level_data, surface):
         self.display_surface = surface
-        self.setup_level(level_data, surface)
+
         self.world_shift = 0
         self.current_x = 0
         # dust
@@ -17,7 +17,7 @@ class Level:
         self.player_on_ground = False
 
         terrain_layout = import_csv_layout(level_data)
-        print(terrain_layout)
+        self.terrain_sprites = self.setup_level(terrain_layout, 'terrain')
 
     # def create_jump_particles(self, pos):
     #     if self.player.sprite.faces_right:
@@ -43,20 +43,23 @@ class Level:
     #             self.player.sprite.rect.midbottom - offset, 'land')
     #         self.dust_sprite.add(fall_dust_particle)
 
-    def setup_level(self, layout, surface):
-        self.tiles = pygame.sprite.Group()
-        self.player = pygame.sprite.GroupSingle()
+    def setup_level(self, layout, type):
+        sprite_group = pygame.sprite.Group()
+
         for row_index, row in enumerate(layout):
-            for col_index, cell in enumerate(row):
-                x = col_index * tile_size
-                y = row_index * tile_size
-                if cell == "X":
-                    tile = Tile((x, y), tile_size)
-                    self.tiles.add(tile)
-                if cell == "P":
-                    player_sprite = Player(
-                        (x, y), surface, self.create_jump_particles)
-                    self.player.add(player_sprite)
+            for col_index, val in enumerate(row):
+                if val != '-1':
+                    x = col_index * tile_size
+                    y = row_index * tile_size
+
+                    if type == 'terrain':
+                        terrain_tile_list = import_cut_graphics(
+                            './graphics/terrain/terrain_tiles.png')
+                        tile_surface = terrain_tile_list[int(val)]
+                        sprite = Tile(tile_size, x, y)
+                        sprite_group.add(sprite)
+
+        return sprite_group
 
     def scroll_x(self):
         player = self.player.sprite
@@ -119,8 +122,8 @@ class Level:
         self.dust_sprite.draw(self.display_surface)
 
         # tiles
-        self.tiles.update(self.world_shift)
-        self.tiles.draw(self.display_surface)
+        self.terrain_sprites.update(self.world_shift)
+        self.terrain_sprites.draw(self.display_surface)
         self.scroll_x()
 
         # player
