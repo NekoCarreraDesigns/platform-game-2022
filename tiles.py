f27c3f90709066121827ae6e2c9ed0778ea0fcd4
NekoCarreraDesigns <denvermaas@gmail.com>
2022-02-28 12:32:55 -0700

    added AnimatedTile class

--- tiles.py
+++ tiles.py
@@ -22,4 +22,10 @@ class Crate(StaticTile):
         super().__init__(size, x, y, pygame.image.load(
             './graphics/terrain/crate.png').convert_alpha())
         offset_y = y + size
-        self.rect = self.image.get_rect(bottomleft = (x, offset_y))
+        self.rect = self.image.get_rect(bottomleft=(x, offset_y))
+
+
+class AnimatedTile(Tile):
+    def __init__(self, size, x, y, path):
+        super().__init__(size, x, y)
+        self.frame = import_folder(path)
