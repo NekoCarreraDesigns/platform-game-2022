b7c669a0f8253d586a82d68a3c1c989585990473
NekoCarreraDesigns <denvermaas@gmail.com>
2022-02-28 12:21:21 -0700

    added offset_y to Crate class method

--- tiles.py
+++ tiles.py
@@ -21,3 +21,5 @@ class Crate(StaticTile):
     def __init__(self, size, x, y):
         super().__init__(size, x, y, pygame.image.load(
             './graphics/terrain/crate.png').convert_alpha())
+        offset_y = y + size
+        self.rect = self.image.get_rect(bottomleft = (x, offset_y))
