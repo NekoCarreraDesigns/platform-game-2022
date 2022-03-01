9620f435840618ad4d5bed4ddf7162bde675e2c2
NekoCarreraDesigns <denvermaas@gmail.com>
2022-03-01 12:38:58 -0700

    fixed spelling error

--- tiles.py
+++ tiles.py
@@ -30,7 +30,7 @@ class Crate(StaticTile):
 class AnimatedTile(Tile):
     def __init__(self, size, x, y, path):
         super().__init__(size, x, y)
-        self.frame = import_folder(path)
+        self.frames = import_folder(path)
         self.frame_index = 0
         self.image = self.frames[self.frame_index]
 
